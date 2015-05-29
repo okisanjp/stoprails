8da1b61bce41441e37784b33d3c46664b0620729
okisanjp <okisan.jp@gmail.com>
2015-05-29 15:24:30 +0900

    mac対応/対話処理他

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 $ chmod +x /PATH/TO/stoprails.sh
 $ ln -s /PATH/TO/stoprails.sh /usr/local/bin/stoprails
 ```
-
--- stoprails.sh
+++ stoprails.sh
@@ -1,6 +1,29 @@
 #!/bin/sh -eu
 
 PIDFILE="tmp/pids/server.pid"
-SERVER_PID=`cat $PIDFILE`
-kill $SERVER_PID
-echo "Stopped WEBRick"
+
+if [ -e $PIDFILE ]; then
+  SERVER_PID=`cat $PIDFILE`
+  PWD=`pwd`
+  if [ `uname` = "Darwin" ]; then
+    /bin/echo -n "[$PWD] Are you sure you want to exit the server? [y/n] > "
+  else
+    echo -n "[$PWD] Are you sure you want to exit the server? [y/n] > "
+  fi
+  while :
+  do
+    read INPUT
+    case "$INPUT" in
+      "y" ) kill $SERVER_PID
+            echo "Stopped WEBRick"
+            break ;;
+      "n" ) echo "Canceled."
+            break ;;
+      * )   echo "[y/n]" ;;
+    esac
+  done
+else
+  echo "$PIDFILE file not exists." 1>&2
+  exit 1
+fi
+exit 0
